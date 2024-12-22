7aaaa0a7d44274f55bf1df1ee35516ea9f00f2e7
aizatnazran <aizatnazran@gmail.com>
2024-12-22 14:51:45 +0800

    fix(start): modify start.sh script for backend container

--- backend/docker/start.sh
+++ backend/docker/start.sh
@@ -8,20 +8,6 @@ chown -R www-data:www-data /var/www/bootstrap/cache
 export APP_ENV=local
 export APP_DEBUG=true
 
-# Clear caches
-php artisan config:clear
-php artisan cache:clear
-php artisan route:clear
-
-# Run migrations if needed
-php artisan migrate --force
-
-# Start Laravel queue worker
-php artisan queue:work &
-
-# Start Laravel scheduler
-php artisan schedule:work &
-
 # Start PHP-FPM
 php-fpm -D
 
