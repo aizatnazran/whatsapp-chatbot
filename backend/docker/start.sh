d8d94ea26834d18dddd2df182950f9cbc7dbb2cd
aizatnazran <aizatnazran@gmail.com>
2024-12-22 14:55:49 +0800

    fix(deploy): fix issues

--- backend/docker/start.sh
+++ backend/docker/start.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # Fix permissions
-chown -R www-data:www-data /var/www/storage
-chown -R www-data:www-data /var/www/bootstrap/cache
+chown -R www-data:www-data /var/www
+chmod -R 755 /var/www/storage
+chmod -R 755 /var/www/bootstrap/cache
+
+# Ensure storage directory exists
+mkdir -p /var/www/storage/framework/sessions
+mkdir -p /var/www/storage/framework/views
+mkdir -p /var/www/storage/framework/cache
+mkdir -p /var/www/storage/logs
 
 # Set Laravel environment
 export APP_ENV=local
